456a141ad5adc46b8fd31a87615860c87cd56912
Akmurza <akuklin@byupathway.edu>
2025-07-22 12:10:26 +0400

    corrected

--- scripts/place.js
+++ scripts/place.js
@@ -1,12 +1,12 @@
 
-// Get current year and last modified date
+// Get  year and last modified 
 document.addEventListener('DOMContentLoaded', function () {
     const currentYear = new Date().getFullYear();
     document.getElementById('currentyear').textContent = currentYear;
     document.getElementById('lastModified').textContent =
         'Last Modification: ' + document.lastModified;
 
-    // Wind chill calculation
+    // wind chill calculation
     const temperature = 8; // °C
     const windSpeed = 15; // km/h
 
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
         return 13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16);
     }
 
-    // Check conditions for wind chill calculation
+    // Check conditions for wind chill
     if (temperature <= 10 && windSpeed > 4.8) {
         const windChill = calculateWindChill(temperature, windSpeed);
         document.getElementById('wind-chill').textContent = Math.round(windChill) + '°C';
